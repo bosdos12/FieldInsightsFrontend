102d62dfa62f5228b6c1329b005853d45bd80df3
Adak Celina <66145209+bosdos12@users.noreply.github.com>
2024-05-24 23:01:32 +0200

    asdas

--- Components/SensorItem.jsx
+++ Components/SensorItem.jsx
@@ -6,32 +6,46 @@ const SensorItem = ({
   sensorID, name, metricIndicator
 }) => {
   const [metricsData, setMetricsData] = useState([]);
+  const [testData, setTestData] = useState([]);
+  const [singleMetricDisplay, setSingleMetricDisplay] = useState([]);
 
   useEffect(() => {
     // Simulate fetching data from API
-    const testData = [
-      { timestamp: "2024-05-01T10:00:00Z", value: 20 },
-      { timestamp: "2024-05-01T11:00:00Z", value: 22 },
-      { timestamp: "2024-05-01T12:00:00Z", value: 21 },
-      { timestamp: "2024-05-01T13:00:00Z", value: 24 },
-      { timestamp: "2024-05-01T14:00:00Z", value: 23 },
-      { timestamp: "2024-05-01T15:00:00Z", value: 25 },
-      { timestamp: "2024-05-01T16:00:00Z", value: 27 }
-    ];
 
-    const formattedData = testData.map((data, index) => {
 
-      const [hours, minutes, seconds] = new Date(data.timestamp).toISOString().substr(11, 8).split(':');
+    // const testData = [
+      // { timestamp: "2024-05-01T10:00:00Z", value: 20 },
+      // { timestamp: "2024-05-01T11:00:00Z", value: 22 },
+      // { timestamp: "2024-05-01T12:00:00Z", value: 21 },
+      // { timestamp: "2024-05-01T13:00:00Z", value: 24 },
+      // { timestamp: "2024-05-01T14:00:00Z", value: 23 },
+      // { timestamp: "2024-05-01T15:00:00Z", value: 25 },
+      // { timestamp: "2024-05-01T16:00:00Z", value: 27 }
+    // ];
 
-      return ({
-        time: `${seconds}:${minutes}:${hours}`,
-        value: data.value
-      })
 
-    });
-
-    setMetricsData(formattedData);
+    setInterval(() => {
+      fetch(`http://localhost:3001/api/getsensorhistory/${sensorID}?pagination=10`).then(async response => {
+        const json_res = await response.json();
+        console.log(json_res);
+        if (response.status === 200) {
+          const formattedData = json_res.map((data, index) => {
+            const [hours, minutes, seconds] = new Date(data.metricTime).toISOString().substr(11, 8).split(':');
+            return ({
+              time: `${seconds}:${minutes}:${hours}`,
+              value: data.metricValue
+            })
+          });
+          setMetricsData(formattedData);
+          setSingleMetricDisplay(formattedData[formattedData.length-1].value);
+        }
+      }).catch(err => {
+        console.log(err);
+        alert("Network connectivity issues.");
+      });
+    }, 3000)
   }, []);
+
 
   return (
     <div className="sensoritem">
@@ -46,7 +60,7 @@ const SensorItem = ({
             {/* <p className="medium__text" style={{ marginTop: "16px" }}>12.12.12</p> */}
           </div>
           <div className="metricarea-livemetric__container">
-            <p className="metricarea-livemetric">50{metricIndicator}</p>
+            <p className="metricarea-livemetric">{singleMetricDisplay} {metricIndicator}</p>
           </div>
           <div className="metricarea-basicdata" style={{
             position: "absolute",
